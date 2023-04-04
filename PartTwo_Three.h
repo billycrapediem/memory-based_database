4d97c12e5585928484177b783df9938b7eed9e3d
jacky0504zyl <yzou17@u.rochester.edu>
2023-04-04 09:50:34 +0800

    join & union

--- PartTwo_Three.h
+++ PartTwo_Three.h
@@ -8,7 +8,7 @@ extern void Navigation_Two_test(Relations x, Relations y);
 extern Relations Union(Relations x, Relations y);
 extern Relations Intersection(Relations x, Relations y);
 extern Relations Difference(Relations x, Relations y);
-extern Relations Join(int pos1, int pos2, Relations x, Relations y);
+extern Relations Join(Relations x, Relations y, int pos1, int pos2);
 extern Relations Selections(int pos, char* name, Relations x);
 extern Relations Projections(int* pos, Relations x);
 #define CSC173_PROJECT4_PARTTWO_THREE_H
