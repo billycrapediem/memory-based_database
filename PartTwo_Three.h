bbc46d0d8950de6a3715189f8e35df27bdcc2bf0
Bryan Zhang <billycrapediem@gmail.com>
2023-04-03 21:52:43 -0400

    Intersection & difference

--- PartTwo_Three.h
+++ PartTwo_Three.h
@@ -4,13 +4,13 @@
 #include "relations.h"
 #ifndef CSC173_PROJECT4_PARTTWO_THREE_H
 extern void Navigation_One_test(Relations x, Relations y);
-extern void Navigation_Two_test(Relations x, Relations y);
+extern void Navigation_Two_test(Relations x, Relations y, Relations z);
 extern Relations Union(Relations x, Relations y);
 extern Relations Intersection(Relations x, Relations y);
 extern Relations Difference(Relations x, Relations y);
 extern Relations Join(int pos1, int pos2, Relations x, Relations y);
 extern Relations Selections(int pos, char* name, Relations x);
-extern Relations Projections(int* pos, Relations x);
+extern Relations Projections(const int* pos, Relations x,int size);
 #define CSC173_PROJECT4_PARTTWO_THREE_H
 
 #endif //CSC173_PROJECT4_PARTTWO_THREE_H
